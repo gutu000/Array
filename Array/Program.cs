dfcf54168b492a2e12d63a43f18ec4f38275d32c
Admin <Admin@TEB106008>
2023-01-13 10:59:23 +0100

    Add project files.

---
+++ Array/Program.cs
@@ -0,0 +1,11 @@
+﻿string[] stad = new string[3];
+stad[0] = "";
+stad[1] = "";
+stad[2] = "";
+
+for (int i = 0; i < 3; i++)
+{
+    Console.WriteLine("Skriv in en stad");
+    stad[i] = Console.ReadLine();
+}
+Console.WriteLine($"Städerna du skrev {stad[0]}, {stad[1]}, {stad[2]}.");
